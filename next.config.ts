277e6eb14cddad42bd28ece433bd9ccca32b500a
Gingyer <wutiaoxun@gmail.com>
2026-05-08 02:12:46 +0900

    standaloneを削除

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  outputFileTracingIncludes: {
-    "/*": ["./node_modules/.prisma/**/*"],
-  },
 };
 
 export default nextConfig;
